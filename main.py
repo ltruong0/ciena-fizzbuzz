f98d8a32258cde017138118e5c5290a49ed19ef1
Lee Truong <lee.t.truong@gmail.com>
2025-05-07 15:19:10 -0500

    fizzbuzz code

---
+++ main.py
@@ -0,0 +1,17 @@
+
+import sys
+
+input_number = int(sys.argv[1])
+
+print(f'number input {input_number}')
+
+for x in range(1,input_number +  1):
+    # print(f'debug {x}')
+    if x % 15 == 0 :
+        print('fizzbuzz')
+    elif x % 5 == 0:
+        print('buzz')
+    elif x % 3 == 0:
+        print('fizz')
+    else:
+        print(x)
